d8e578b45218bd94e7f9b748c2eb41c7173a40b4
Alt B 2022 <massie.w@gmail.com>
2023-03-18 21:00:50 -0700

    Added code to module: rules for webpack.config to use babel loader and css style loader.  Ran successful npm start

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,4 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
@@ -55,6 +54,22 @@ module.exports = () => {
     module: {
       rules: [
         //TODO: Need to add rules here
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
